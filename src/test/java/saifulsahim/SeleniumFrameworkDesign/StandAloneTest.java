809dba6b54166ceb135a87899a8145960968690f
Saiful Alam Sahim <sahimalam96@gmail.com>
2025-09-21 13:44:15 +0600

    Login to verify the items in cart using stream then click checkout

--- src/test/java/saifulsahim/SeleniumFrameworkDesign/StandAloneTest.java
+++ src/test/java/saifulsahim/SeleniumFrameworkDesign/StandAloneTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import java.time.Duration;
 import java.util.HashMap;
@@ -21,6 +22,7 @@ public class StandAloneTest {
 
 	public static void main(String[] args) {
 		
+		String productName = "ZARA COAT 3";
 		WebDriverManager.chromedriver().setup(); // Instead of setting WebDriver locally, it is managed through the WebDriverManager Maven repository.
 		
 		// to prevent google password prompt popup below these two lines added
@@ -45,7 +47,7 @@ public class StandAloneTest {
 		// get all products list
 		List<WebElement> products = driver.findElements(By.cssSelector(".mb-3"));
 		// Using java 8 stream i.e kind of loop 
-		WebElement prod = products.stream().filter(product->product.findElement(By.cssSelector("b")).getText().equals("ZARA COAT 3")).findFirst().orElse(null);
+		WebElement prod = products.stream().filter(product->product.findElement(By.cssSelector("b")).getText().equals(productName)).findFirst().orElse(null);
 		// click last button out of 2 buttons (View, add to cart)
 		prod.findElement(By.cssSelector(".card-body button:last-of-type")).click(); 
 		
@@ -53,8 +55,15 @@ public class StandAloneTest {
 		// Waiting for the animation class to disappear by passing the complete WebElement.
 		// Using invisibilityOf instead of visibilityOfElementLocated to improve performance.		
 		wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating")))); 
-		// Using custom css selector and also using regular expression
+		// Using custom css selector [attribute = 'value'] and also using regular expression
 		driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
+		
+		List<WebElement> cartProducts = driver.findElements(By.cssSelector(".cartSection h3")); // xpath using css class //*[@class='cartSection']
+		Boolean match = cartProducts.stream().anyMatch(cartProduct->cartProduct.getText().equalsIgnoreCase(productName));
+		Assert.assertTrue(match);
+		driver.findElement(By.cssSelector(".totalRow button")).click();
 	}
+	
+	
 
 }
